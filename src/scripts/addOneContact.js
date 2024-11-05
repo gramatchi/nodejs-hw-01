f18a832cac54af2f2706aa515a8a453aba0ecdcd
gramatchi <nik98.champion@gmail.com>
2024-11-05 02:36:04 +0100

    add add one contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,13 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+    const contactsList = await readContacts();
+    
+    const newContacts =  createFakeContact();
+
+    await writeContacts([...contactsList, newContacts]);
+};
 
 addOneContact();
